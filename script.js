133838a97829328d7ded9df7cab6051d4c4636bf
KOTIK\user <ryuminaen@gmail.com>
2021-12-12 15:13:47 +0700

    les 2

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('Hello World!')
+let num;
+let score;
+
+const question_1 = {
+  question: "", 
+  answer: "",
+  correctAnswer: ""
+}
+
+const question_2 = {
+  question: "", 
+  answer: "",
+  correctAnswer: ""
+}
+
+const question_3 = {
+  question: "", 
+  answer: "",
+  correctAnswer: ""
+}
+
+const question_4 = {
+  question: "", 
+  answer: "",
+  correctAnswer: ""
+}
