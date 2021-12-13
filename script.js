09b23ed2f4128a0a6d04c2b40267392238c8000f
KOTIK\user <ryuminaen@gmail.com>
2021-12-13 23:05:39 +0700

    fix

--- script.js
+++ script.js
@@ -2,28 +2,53 @@ let num = 0;
 let score;
 
 const quiz = [
-  {
-    question: 1, 
-    answer: true,
-    correctAnswer: "c"
+  
+  question1 = {
+    question: "Как зовут моего кота?", 
+    answer: "c",
+    correctAnswer: "c",
+    varsOfAnswers: {
+      a: "Борис",
+      b: "Василий",
+      c: "Леонид",
+      d: "Иннокентий"
+    }
   },
 
-  {
-    question: 2, 
-    answer: false,
-    correctAnswer: "b"
+  question2 = {
+    question: "Столица Непала?", 
+    answer: "b",
+    correctAnswer: "d",
+    varsOfAnswers: {
+      a: "Тибет",
+      b: "Эверест",
+      c: "Горы",
+      d: "Катманду"
+    }
   },
 
-  {
-    question: 3, 
-    answer: false,
-    correctAnswer: "a"
+  question3 = {
+    question: "Где живет дед Мороз?", 
+    answer: "c",
+    correctAnswer: "c",
+    varsOfAnswers: {
+      a: "Ямайка",
+      b: "Томск",
+      c: "Северный полюс",
+      d: "не знаю"
+    }
   },
 
-  {
-    question: 4, 
-    answer: true,
-    correctAnswer: "c"
+  question4 = {
+    question: "Эта игра интересная?", 
+    answer: "b",
+    correctAnswer: "a",
+    varsOfAnswers: {
+      a: "возможно",
+      b: "нет",
+      c: "да",
+      d: "не знаю"
+    }
   }
 ]
 
